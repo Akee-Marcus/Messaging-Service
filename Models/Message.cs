18c82a7da8e4faee88a695b485405a7036438fa5
Akee-Marcus <149623047+Akee-Marcus@users.noreply.github.com>
2025-10-17 14:34:43 -0400

    Version 0.1.0 (#10)
    * Initial Push
    
    Controller template
    
    * Version 0.1.0 (#9)
    
    * initial push for messaging service
    
    * Sending messages
    
    * Addition of the first tables + columns
    
    Adding the beginning of the tables and columns to sort the messages into, will later connect to the database.
    
    * chore: added gitignore
    
    * chore: removed binary files
    
    * net update
    
    updated net to 9.0
    
    * docker addition
    
    Added dockerfile and docker-compose file
    
    ---------
    
    Co-authored-by: ChernoJ123 <149393142+ChernoJ123@users.noreply.github.com>
    Co-authored-by: TBush05 <tclb24@gmail.com>
    Co-authored-by: Dylan <149787554+dylanmxkenzie@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: ChernoJ123 <149393142+ChernoJ123@users.noreply.github.com>
    Co-authored-by: TBush05 <tclb24@gmail.com>
    Co-authored-by: Dylan <149787554+dylanmxkenzie@users.noreply.github.com>

---
+++ Models/Message.cs
@@ -0,0 +1,10 @@
+namespace ChatService.Models;
+
+public class Message
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? SenderId { get; set; }
+    public string? ReceiverId { get; set; }
+    public string? Content { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+// Program.cs
+using ChatService.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<MessagesRepository>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
