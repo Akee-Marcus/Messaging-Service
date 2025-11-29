a6f2196a0ee7ca2d8278c715a3fffffb8bb3e8a8
Dylan <149787554+dylanmxkenzie@users.noreply.github.com>
2025-11-28 20:22:31 -0500

    Testing added

--- test/MessagingService.Domain.Tests/UnitTest1.cs
+++ test/MessagingService.Domain.Tests/UnitTest1.cs
@@ -1,10 +1,65 @@
-﻿namespace MessagingService.Domain.Tests;
+﻿using System;
+using ChatService.Models;
+using Xunit;
 
-public class UnitTest1
+namespace MessagingService.Tests
 {
-    [Fact]
-    public void Test1()
+    public class MessageTests
     {
+        [Fact]
+        public void Message_ShouldAutoGenerateId()
+        {
+            var m1 = new Message();
+            var m2 = new Message();
 
+            Assert.NotEqual(Guid.Empty, m1.Id);
+            Assert.NotEqual(Guid.Empty, m2.Id);
+            Assert.NotEqual(m1.Id, m2.Id);
+        }
+
+        [Fact]
+        public void Message_ShouldAutoGenerateTimestamp()
+        {
+            var msg = new Message();
+
+            Assert.True((DateTime.UtcNow - msg.Timestamp).TotalSeconds < 2);
+        }
+
+        [Fact]
+        public void Message_ShouldStoreProperties()
+        {
+            var msg = new Message
+            {
+                SenderId = "UserA",
+                ReceiverId = "UserB",
+                Content = "Hello!"
+            };
+
+            Assert.Equal("UserA", msg.SenderId);
+            Assert.Equal("UserB", msg.ReceiverId);
+            Assert.Equal("Hello!", msg.Content);
+        }
+
+        [Fact]
+        public void Message_PropertiesShouldBeSettable()
+        {
+            var id = Guid.NewGuid();
+            var time = new DateTime(2020, 1, 1);
+
+            var msg = new Message
+            {
+                Id = id,
+                Timestamp = time,
+                SenderId = "A",
+                ReceiverId = "B",
+                Content = "Test"
+            };
+
+            Assert.Equal(id, msg.Id);
+            Assert.Equal("A", msg.SenderId);
+            Assert.Equal("B", msg.ReceiverId);
+            Assert.Equal("Test", msg.Content);
+            Assert.Equal(time, msg.Timestamp);
+        }
     }
 }
