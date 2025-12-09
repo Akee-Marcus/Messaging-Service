ed070e6562baa87cf5c7d16d8070a1ee1fabdef7
TBush05 <tclb24@gmail.com>
2025-12-08 19:42:19 -0500

    .

--- src/MessagingService.Presentation/Program.cs
+++ src/MessagingService.Presentation/Program.cs
@@ -10,15 +10,33 @@ builder.Services.AddSingleton<UsersRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+         options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Chat Service API",
+        Version = "v1"
+    });
+    
+        var xmlFilename = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    }
+);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(
+        options => // UseSwaggerUI is called only in Development.
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    }
+    );
 
 }
 
